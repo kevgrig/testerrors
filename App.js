67895ab2902f3a329b93ef29b617959ffce113a0
Kevin <kevin@myplaceonline.com>
2021-06-18 11:57:45 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+
+function Padding(props) {
+  return <View style={{ padding: 10 }}>{props.children}</View>;
+}
 
 export default function App() {
+  onClickNoCatchSync = () => {
+    console.log(new URL("https://example.com/").hostname);
+  }
+
+  onClickNoCatchAsync = async () => {
+    console.log(new URL("https://example.com/").hostname);
+  }
+
+  onClickCatchSyncConsoleLog = () => {
+    try {
+      console.log(new URL("https://example.com/").hostname);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  onClickCatchAsyncConsoleLog = async () => {
+    try {
+      console.log(new URL("https://example.com/").hostname);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  onClickCatchSyncCustomLog = () => {
+    try {
+      console.log(new URL("https://example.com/").hostname);
+    } catch (e) {
+      console.log("Introspected error: " + this.getExceptionDetails(e));
+    }
+  }
+
+  onClickCatchAsyncCustomLog = async () => {
+    try {
+      console.log(new URL("https://example.com/").hostname);
+    } catch (e) {
+      console.log("Introspected error: " + this.getExceptionDetails(e));
+    }
+  }
+
+  getExceptionDetails = (e) => {
+    let result = "== START ==\n\ntoString(): " + e + "\n\nstack: " + e.stack;
+    result += "\n\nclass: " + e.constructor.name;
+    result += "\n\nmessage: " + e.message;
+    result += "\n\nname: " + e.name;
+    result += "\n\nfileName: " + e.fileName;
+    result += "\n\nlineNumber: " + e.lineNumber;
+    result += "\n\ndescription: " + e.description;
+    result += "\n\nproperties:\n";
+    for (var prop in e) {
+      result += "  [" + prop + "]: ";
+      try {
+        if (typeof(e[prop]) == "function") {
+          result += "function(): ";
+        } else {
+          result += e[prop];
+        }
+      } catch (propError) {
+        result += "_inaccessible_";
+      }
+      result += "\n";
+    }
+    result += "\n== END ==";
+    return result;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Hello World</Text>
+      <Padding><Button onPress={this.onClickNoCatchSync} title="onClick no-catch sync" /></Padding>
+      <Padding><Button onPress={this.onClickNoCatchAsync} title="onClick no-catch async" /></Padding>
+      <Padding><Button onPress={this.onClickCatchSyncConsoleLog} title="onClick catch sync console log" /></Padding>
+      <Padding><Button onPress={this.onClickCatchAsyncConsoleLog} title="onClick catch async console log" /></Padding>
+      <Padding><Button onPress={this.onClickCatchSyncCustomLog} title="onClick catch sync custom log" /></Padding>
+      <Padding><Button onPress={this.onClickCatchAsyncCustomLog} title="onClick catch async custom log" /></Padding>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
